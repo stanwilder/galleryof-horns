dd04689548b2cfc4c1dc54eead7cf8198307ccc2
Stanley <stanley.wilder.24@gmail.com>
2022-05-09 15:51:47 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import App from './App.css';
+import Main from './Main.js';
+import Header from './Header.js'
+import Footer from './Footer.js'
 
-function App() {
+class App extends react 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+  )    
 export default App;
