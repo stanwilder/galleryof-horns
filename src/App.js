629eb0b5db8141b0d95a995fba95595304bc3013
Stanley <stanley.wilder.24@gmail.com>
2022-05-09 17:35:37 -0700

    finished lab 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import App from './App.css';
+import React from 'react'
 import Main from './Main.js';
 import Header from './Header.js'
 import Footer from './Footer.js'
+// import HornedBeast from './HornedBeast.js';
 
-class App extends react 
-  return (
-  )    
+class App extends React.Component {
+  
+  
+  render () {
+
+    return (
+      <>
+      <Header />
+      
+      <Main />
+      
+
+      <Footer />
+      
+      </>
+      
+      )    
+    }
+}  
 export default App;
---
+++ src/HornedBeast.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+
+  render() {
+    return(
+      <>
+      <h2>{this.props.title}</h2>
+      <img src = {this.props.url} alt = {this.props.title} title = {this.props.title} />
+
+      <p>{this.props.description}</p>
+      
+      </>
+    )}
+  
+  
+  
+  
+  
+  
+}
+export default HornedBeast; 
