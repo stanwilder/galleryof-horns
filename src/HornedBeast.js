079a17552743bcd98e46a03cf2cdb4c4bc1ad3cc
Stanley <stanley.wilder.24@gmail.com>
2022-05-10 19:36:36 -0700

    Finished lab 02

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import {Card, Button} from 'react-bootstrap';
+import './Hornedbeast.css'
 
 class HornedBeast extends React.Component {
+  constructor(props)
+ {
+   super(props);
+   this.state = {
+     likes: 0
+   }
+ }
 
+ handleLikes = () => {
+   this.setState({
+     likes: this.state.likes + 1
+   })
+ }
   render() {
     return(
-      <>
-      <h2>{this.props.title}</h2>
-      <img src = {this.props.url} alt = {this.props.title} title = {this.props.title} />
+      <Card 
+      style={{width: '18rem', margin: '10px', padding: '10px', borderRadius: '10px', borderColor: 'black', display: 'inline-block'}}
 
-      <p>{this.props.description}</p>
-      
-      </>
+    // help from Cassie and Garfield (TA's)
+      >
+         <Card.Body>
+         <Card.Img
+         src = {this.props.url} 
+         alt = {this.props.title}
+         title = {this.props.title}
+         />
+        <Card.Title>
+        {this.props.title}
+        </Card.Title>
+        <Card.Text>
+          {this.state.likes} Like this? Click Here!❤️ 
+        </Card.Text>
+        <Button onClick={this.handleLikes}
+        > CLICK ME
+
+        </Button>
+         </Card.Body>
+      </Card>
     )}
   
   
